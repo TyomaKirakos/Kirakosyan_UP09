e98650040f30724b18d8554df62cb76f01906c45
TyomaKirakos <98010508+TyomaKirakos@users.noreply.github.com>
2022-04-19 10:31:02 +0300

    made a from validation

--- js/modules/formValidation.js
+++ js/modules/formValidation.js
@@ -1,13 +1,24 @@
 const formValidation = () => {
     const phoneInputs = document.querySelectorAll('.phone-user');
+    const nameInputs = document.querySelectorAll('input[name="user_name"]');
     const numbersPattern = /^[1-9]+[0-9]*$/;
-    const phonePattern = /^((8|\+374|\+994|\+995|\+375|\+7|\+380|\+38|\+996|\+998|\+993)[\- ]?)?\(?\d{3,5}\)?[\- ]?\d{1}[\- ]?\d{1}[\- ]?\d{1}[\- ]?\d{1}[\- ]?\d{1}(([\- ]?\d{1})?[\- ]?\d{1})?$/;
+    // const phonePattern = /^((8|\+374|\+994|\+995|\+375|\+7|\+380|\+38|\+996|\+998|\+993)[\- ]?)?\(?\d{3,5}\)?[\- ]?\d{1}[\- ]?\d{1}[\- ]?\d{1}[\- ]?\d{1}[\- ]?\d{1}(([\- ]?\d{1})?[\- ]?\d{1})?$/;
+    const namePatternt = /^[а-яё -]+$/i;
+
+    console.log(nameInputs);
 
     phoneInputs.forEach((phoneInput) => {
-        console.log(phoneInput);
         phoneInput.addEventListener('input', () => {
             if(!numbersPattern.test(phoneInput.value[phoneInput.value.length-1])){
                 phoneInput.value = phoneInput.value.slice(0, -1);
+            }
+        })
+    })
+
+    nameInputs.forEach((nameInput) => {
+        nameInput.addEventListener('input', () => {
+            if(!namePatternt.test(nameInput.value[nameInput.value.length-1])){
+                nameInput.value = nameInput.value.slice(0, -1);
             }
         })
     })
